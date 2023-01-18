adc2ee47fe0edea06162b097deb81050782c24d3
jschiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2023-01-17 22:36:59 -0500

    remove double home page link header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,6 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "My Site",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
